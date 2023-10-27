f51e84f6c16285126ec4a0f1aa11709c9a3b35d8
Kartikey Tandon <tandonkartiey11@gmail.com>
2023-10-27 18:20:50 +0530

    Integrated API + Some Bug fixes

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,29 +1,18 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function MushroomForm() {
   const [formData, setFormData] = useState({
-    capShape: '',
-    capSurface: '',
-    capColor: '',
+    cap_color: '',
     bruises: '',
     odor: '',
-    gillAttachment: '',
-    gillSpacing: '',
-    gillSize: '',
-    gillColor: '',
-    stalkShape: '',
-    stalkRoot: '',
-    stalkSurfaceAboveRing: '',
-    stalkSurfaceBelowRing: '',
-    stalkColorAboveRing: '',
-    stalkColorBelowRing: '',
-    veilType: '',
-    veilColor: '',
-    ringNumber: '',
-    ringType: '',
-    sporePrintColor: '',
+    gill_size: '',
+    gill_color: '',
+    stalk_surface_above_ring: '',
+    stalk_surface_below_ring: '',
+    ring_type: '',
+    spore_print_color: '',
     population: '',
-    habitat: '',
   });
 
   const handleChange = (e) => {
@@ -37,280 +26,205 @@ function MushroomForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
+    
+    axios.post('https://mushroomclass-mycb.onrender.com/prediction', formData)
+    .then((response) => {
+      console.log('Response:', response.data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   };
 
   return (
     <div className="max-w-md mx-auto p-4 bg-gray-100 border rounded-lg shadow">
       <h2 className="text-2xl text-center mb-4">Mushroom Characteristics</h2>
       <form onSubmit={handleSubmit}>
-        {/* Cap Shape */}
-        <div className="mb-4">
-          <label className="block font-semibold">Cap Shape:</label>
-          <input
-            type="text"
-            name="capShape"
-            value={formData.capShape}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Cap Surface */}
-        <div className="mb-4">
-          <label className="block font-semibold">Cap Surface:</label>
-          <input
-            type="text"
-            name="capSurface"
-            value={formData.capSurface}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Cap Color */}
-        <div className="mb-4">
+      <div className="mb-4">
           <label className="block font-semibold">Cap Color:</label>
-          <input
-            type="text"
-            name="capColor"
-            value={formData.capColor}
+          <select
+            name="cap_color"
+            value={formData.cap_color}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
+          >
+            <option value="n">n</option>
+            <option value="y">y</option>
+            <option value="w">w</option>
+            <option value="g">g</option>
+            <option value="e">e</option>
+            <option value="p">p</option>
+            <option value="b">b</option>
+            <option value="u">u</option>
+            <option value="c">c</option>
+            <option value="r">r</option>
+          </select>
         </div>
 
-        {/* Bruises */}
+
         <div className="mb-4">
           <label className="block font-semibold">Bruises:</label>
-          <input
-            type="text"
+          <select
             name="bruises"
             value={formData.bruises}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
+          >
+            <option value="t">t</option>
+            <option value="f">f</option>
+          </select>
         </div>
+
 
         {/* Odor */}
         <div className="mb-4">
           <label className="block font-semibold">Odor:</label>
-          <input
-            type="text"
+          <select
             name="odor"
             value={formData.odor}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Gill Attachment */}
-        <div className="mb-4">
-          <label className="block font-semibold">Gill Attachment:</label>
-          <input
-            type="text"
-            name="gillAttachment"
-            value={formData.gillAttachment}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Gill Spacing */}
-        <div className="mb-4">
-          <label className="block font-semibold">Gill Spacing:</label>
-          <input
-            type="text"
-            name="gillSpacing"
-            value={formData.gillSpacing}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
+          >
+            <option value="p">p</option>
+            <option value="a">a</option>
+            <option value="l">l</option>
+            <option value="n">n</option>
+            <option value="f">f</option>
+            <option value="c">c</option>
+            <option value="y">y</option>
+            <option value="s">s</option>
+            <option value="m">m</option>
+          </select>
         </div>
 
         {/* Gill Size */}
         <div className="mb-4">
           <label className="block font-semibold">Gill Size:</label>
-          <input
-            type="text"
-            name="gillSize"
-            value={formData.gillSize}
+          <select
+            name="gill_size"
+            value={formData.gill_size}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
+          >
+            <option value="n">n</option>
+            <option value="b">b</option>
+          </select>
         </div>
 
         {/* Gill Color */}
         <div className="mb-4">
           <label className="block font-semibold">Gill Color:</label>
-          <input
-            type="text"
-            name="gillColor"
-            value={formData.gillColor}
+          <select
+            name="gill_color"
+            value={formData.gill_color}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Stalk Shape */}
-        <div className="mb-4">
-          <label className="block font-semibold">Stalk Shape:</label>
-          <input
-            type="text"
-            name="stalkShape"
-            value={formData.stalkShape}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Stalk Root */}
-        <div className="mb-4">
-          <label className="block font-semibold">Stalk Root:</label>
-          <input
-            type="text"
-            name="stalkRoot"
-            value={formData.stalkRoot}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
+          >
+            <option value="k">k</option>
+            <option value="n">n</option>
+            <option value="g">g</option>
+            <option value="p">p</option>
+            <option value="w">w</option>
+            <option value="h">h</option>
+            <option value="u">u</option>
+            <option value="b">b</option>
+            <option value="r">r</option>
+            <option value="y">y</option>
+          </select>
         </div>
 
         {/* Stalk Surface Above Ring */}
         <div className="mb-4">
           <label className="block font-semibold">Stalk Surface Above Ring:</label>
-          <input
-            type="text"
-            name="stalkSurfaceAboveRing"
-            value={formData.stalkSurfaceAboveRing}
+          <select
+            name="stalk_surface_above_ring"
+            value={formData.stalk_surface_above_ring}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
+          >
+            <option value="s">s</option>
+            <option value="f">f</option>
+            <option value="k">k</option>
+            <option value="y">y</option>
+          </select>
         </div>
 
         {/* Stalk Surface Below Ring */}
         <div className="mb-4">
           <label className="block font-semibold">Stalk Surface Below Ring:</label>
-          <input
-            type="text"
-            name="stalkSurfaceBelowRing"
-            value={formData.stalkSurfaceBelowRing}
+          <select
+            name="stalk_surface_below_ring"
+            value={formData.stalk_surface_below_ring}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Stalk Color Above Ring */}
-        <div className="mb-4">
-          <label className="block font-semibold">Stalk Color Above Ring:</label>
-          <input
-            type="text"
-            name="stalkColorAboveRing"
-            value={formData.stalkColorAboveRing}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Stalk Color Below Ring */}
-        <div className="mb-4">
-          <label className="block font-semibold">Stalk Color Below Ring:</label>
-          <input
-            type="text"
-            name="stalkColorBelowRing"
-            value={formData.stalkColorBelowRing}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Veil Type */}
-        <div className="mb-4">
-          <label className="block font-semibold">Veil Type:</label>
-          <input
-            type="text"
-            name="veilType"
-            value={formData.veilType}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Veil Color */}
-        <div className="mb-4">
-          <label className="block font-semibold">Veil Color:</label>
-          <input
-            type="text"
-            name="veilColor"
-            value={formData.veilColor}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Ring Number */}
-        <div className="mb-4">
-          <label className="block font-semibold">Ring Number:</label>
-          <input
-            type="text"
-            name="ringNumber"
-            value={formData.ringNumber}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
+          >
+            <option value="s">s</option>
+            <option value="f">f</option>
+            <option value="k">k</option>
+            <option value="y">y</option>
+          </select>
         </div>
 
         {/* Ring Type */}
         <div className="mb-4">
           <label className="block font-semibold">Ring Type:</label>
-          <input
-            type="text"
-            name="ringType"
-            value={formData.ringType}
+          <select
+            name="ring_type"
+            value={formData.ring_type}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
+          >
+            <option value="p">p</option>
+            <option value="e">e</option>
+            <option value="l">l</option>
+            <option value="f">f</option>
+            <option value="n">n</option>
+          </select>
         </div>
 
         {/* Spore Print Color */}
         <div className="mb-4">
           <label className="block font-semibold">Spore Print Color:</label>
-          <input
-            type="text"
-            name="sporePrintColor"
-            value={formData.sporePrintColor}
+          <select
+            name="spore_print_color"
+            value={formData.spore_print_color}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
+          >
+            <option value="k">k</option>
+            <option value="n">n</option>
+            <option value="u">u</option>
+            <option value="h">h</option>
+            <option value="w">w</option>
+            <option value="r">r</option>
+            <option value="o">o</option>
+            <option value="y">y</option>
+            <option value="b">b</option>
+          </select>
         </div>
 
         {/* Population */}
         <div className="mb-4">
           <label className="block font-semibold">Population:</label>
-          <input
-            type="text"
+          <select
             name="population"
             value={formData.population}
             onChange={handleChange}
             className="w-full p-2 border rounded"
-          />
-        </div>
-
-        {/* Habitat */}
-        <div className="mb-4">
-          <label className="block font-semibold">Habitat:</label>
-          <input
-            type="text"
-            name="habitat"
-            value={formData.habitat}
-            onChange={handleChange}
-            className="w-full p-2 border rounded"
-          />
+          >
+            <option value="s">s</option>
+            <option value="n">n</option>
+            <option value="a">a</option>
+            <option value="v">v</option>
+            <option value="y">y</option>
+            <option value="c">c</option>
+          </select>
         </div>
 
         <button
           type="submit"
           className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-        >
+        > 
           Submit
         </button>
       </form>
